943f7f8a82ae126e1a415f4bb4f4a77bbcb09678
atulrana007 <atul.rana@zopsmart.com>
2021-10-19 11:22:10 +0530

    update the Account context

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     main: lModules,
   },
   output: {
-    filename: "bundle.min.js",
+    filename: "bundle_3.min.js",
     path: path.resolve(__dirname, "dist"),
   },
   module: {
